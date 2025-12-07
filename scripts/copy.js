b9a5fdf08387fa35122a35f7315d4acdbfa0bea2
Oleg Shilov <shilov-1@yandex.ru>
2025-12-07 12:28:39 +0300

    copy to m-days-public;

--- scripts/copy.js
+++ scripts/copy.js
@@ -4,3 +4,4 @@ fs.rmSync('../m-days-public', { recursive: true });
 fs.mkdirSync('../m-days-public');
 
 fs.cpSync('./dist', '../m-days-public', { recursive: true });
+fs.cpSync('../m-days-legacy', '../m-days-public/l', { recursive: true });
